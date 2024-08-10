98d998aeec8e270c4609bf8dda0a506752a0f83b
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2024-08-10 04:58:23 +0200

    simplify things

--- find_svg.mjs
+++ find_svg.mjs
@@ -38,7 +38,7 @@ for (const file of files) {
 				if (node.type === Syntax.CallExpression && node.callee?.property?.name === 'createElement' && node.arguments?.[0].value === 'svg') {
 					// as i understand it we don't want to go deeper if it's an svg (bc there can be svg in svg but we're only interested in the one most "outside")
 					this.skip();
-					const svg = createSvg(node);
+					const svg = (createSvgBody(node)).end({ prettyPrint: true });
 					const hash = createHash('sha1').update(svg).digest('hex').substring(0,16);
 					console.log("Debug hash", hash);
 					console.log("Debug svg", svg);
@@ -54,68 +54,26 @@ for (const file of files) {
 	}
 }
 
-// return svg as string from the node given assumed as a valid svg
-function createSvg(node) {
-	console.log("Debug ", node.type, Object.keys(node), node.expression, node.arguments, Object.keys(node.callee), node.callee);
-	const svg = create().ele('svg');
 
-	node.arguments[1].properties.forEach((prop) => {
+function createSvgBody(node, xml = undefined) {
+	if (!xml) {
+		xml = create();
+	}
+	if (!node) {
+		return xml;
+	}
+	const elem = xml.ele(node.arguments[0].value);
+	node.arguments[1].properties?.forEach((prop) => {
 		if (prop.type === 'SpreadElement' || prop.key.name === 'className') return;
-		// console.log("Debug prop", prop.type, prop.key.name, prop.value.value);
-		svg.att(fixSVGKeyName(prop.key), prop.value.value);
+		elem.att(fixSVGKeyName(prop.key) , prop.value.value);
 	})
-
-	node.arguments.slice(2).forEach((prop) => {
+	node.arguments.slice(2)?.forEach((prop) => {
 		if (prop.type === 'CallExpression') {
-			//console.log("Debug prop", prop, prop.property);
-			if (prop.callee.property.name === 'createElement') {
-				if (prop.arguments[0].value === 'path') {
-					//console.debug("Debug path", prop.arguments[1]);
-					const path = svg.ele('path');
-					prop.arguments[1].properties.forEach((prop) => {
-						console.debug("Debug prop", prop, prop.key.name || prop.key.value , prop.value.value);
-						path.att(fixSVGKeyName(prop.key) , prop.value.value);
-					})
-				} else if (prop.arguments[0].value === 'g') {
-					const g = svg.ele('g');
-					prop.arguments[1]?.properties?.forEach((prop) => {
-						console.debug("Debug prop", prop, prop.key.name || prop.key.value , prop.value.value);
-						g.att(fixSVGKeyName(prop.key) , prop.value.value);
-					})
-					// TODO: refactor this to have a recursive function and not have dupe code
-					prop.arguments.slice(2).forEach((prop) => {
-						if (prop.type === 'CallExpression') {
-							if (prop.callee.property.name === 'createElement') {
-								if (prop.arguments[0].value === 'path') {
-									const path = g.ele('path');
-									prop.arguments[1].properties.forEach((prop) => {
-										console.debug("Debug prop", prop, prop.key.name || prop.key.value , prop.value.value);
-										path.att(fixSVGKeyName(prop.key) , prop.value.value);
-									})
-								}
-							}
-						}
-					});
-				}
-				else if (prop.arguments[0].value === 'rect') {
-					const rect = svg.ele('rect');
-					prop.arguments[1].properties.forEach((prop) => {
-						console.debug("Debug prop", prop, prop.key.name || prop.key.value , prop.value.value);
-						rect.att(fixSVGKeyName(prop.key) , prop.value.value);
-					})
+			createSvgBody(prop, elem);
+		}
+	});
 
-				} else {
-					console.log("Unknown element", prop.arguments[0].value);
-				}
-				// TODO: we are not handling defs elements
-
-			} else {
-				console.warn("unexpected element", prop.type);
-			}
-		}});
-
-	return svg.end({ prettyPrint: true });
-
+	return xml;
 }
 
 // fix svg key names that aren't the same in html vs xml
