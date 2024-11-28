b4e53d555996adf7f54a70027d37245bdb734ec1
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2024-11-28 19:59:56 +0100

    also consider new files as a change

--- update.sh
+++ update.sh
@@ -3,10 +3,12 @@
 UpdateSvg() {
     node find_svg.mjs
     changes=$(git ls-files -s $(git ls-files -m) | sed -e '/^16/d' )
+    # let's assume any untracked files we have is something we want to add
+    new=$(git ls-files -o --exclude-standard)
     if [ "$PULL_REQUEST" == "true" ]; then
         echo "Pull request detected"
         echo "Changes: $(echo \"$changes\" | wc -l) files"
-    elif [ -n "$changes" ]; then
+    elif [ -n "$changes" -o -n "$new" ]; then
         echo "Changes detected"
         git add -A
         git commit -S -a -m "$(git status --porcelain | wc -l) svgs | $(git status --porcelain|awk '{print "basename " $2}'| sh | sed '{:q;N;s/\n/, /g;t q}')"
